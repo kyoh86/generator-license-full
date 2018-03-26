fa51db2dd81d258e1c9f23c4613405457031e204
Kyoichiro Yamada <yamada@wacul.co.jp>
2018-03-27 00:21:15 +0900

    support 'license' option

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,55 +1,56 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const simpleLics = require('spdx-license-list/simple');
-const fulLics = require('spdx-license-list/full');
-const autocomplete = require('inquirer-autocomplete-prompt');
-const Fuse = require('fuse.js');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const simpleLics = require("spdx-license-list/simple");
+const fulLics = require("spdx-license-list/full");
+const autocomplete = require("inquirer-autocomplete-prompt");
+const Fuse = require("fuse.js");
 
 module.exports = class extends Generator {
   constructor(...args) {
     super(...args);
-    this.env.adapter.promptModule.registerPrompt('autocomplete', autocomplete);
+    this.env.adapter.promptModule.registerPrompt("autocomplete", autocomplete);
   }
 
   prompting() {
     // Have Yeoman greet the user.
     if (!this.options.silent) {
-      let name = chalk.red('generator-license-full');
+      let name = chalk.red("generator-license-full");
       this.log(yosay(`Welcome to the funkadelic ${name} generator!`));
     }
 
-    let choices = [];
-    simpleLics.forEach(id => {
-      let obj = fulLics[id];
-      obj.id = id;
-      choices.push({ name: id, value: obj });
-    });
-    let fuseOptions = {
-      shouldSort: true,
-      threshold: 0.5,
-      location: 0,
-      distance: 100,
-      maxPatternLength: 32,
-      minMatchCharLength: 2,
-      keys: ['name']
-    };
-    let fuse = new Fuse(choices, fuseOptions); // "list" is the item array
-    const prompts = [
-      {
-        type: 'autocomplete',
-        name: 'license',
-        message: 'Choose a license template',
-        default: 'MIT',
+    let prompts = [];
+    if (this.options.license === undefined) {
+      let choices = [];
+      simpleLics.forEach(id => {
+        let obj = fulLics[id];
+        obj.id = id;
+        choices.push({ name: id, value: obj });
+      });
+      let fuseOptions = {
+        shouldSort: true,
+        threshold: 0.5,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 2,
+        keys: ["name"]
+      };
+      let fuse = new Fuse(choices, fuseOptions); // "list" is the item array
+      prompts.push({
+        type: "autocomplete",
+        name: "license",
+        message: "Choose a license template",
+        default: "MIT",
         source: (answersSoFar, input) => {
           if (!input) {
             return Promise.resolve(choices);
           }
           return Promise.resolve(fuse.search(input));
         }
-      }
-    ];
+      });
+    }
 
     return this.prompt(prompts).then(props => {
       this.props = props;
@@ -58,8 +59,8 @@ module.exports = class extends Generator {
 
   writing() {
     const caption = /^(#+)\s*license$/i;
-    let readme = this.fs.read('README.md', { defaults: '' });
-    let level = '';
+    let readme = this.fs.read("README.md", { defaults: "" });
+    let level = "";
     let next = null;
     let remove = false;
     let newlines = [];
@@ -69,21 +70,23 @@ module.exports = class extends Generator {
       if (inserted) {
         return;
       }
-      if (level === '') {
-        level = '#';
+      if (level === "") {
+        level = "#";
       }
-      newlines.push(level + ' LICENSE');
-      newlines.push('');
+      newlines.push(level + " LICENSE");
+      newlines.push("");
       newlines.push(
-        '[![' +
+        "[![" +
           l.name +
-          '](http://img.shields.io/badge/license-' +
-          encodeURIComponent(l.id).replace('-', '--') +
-          '-blue.svg)](' +
+          "](http://img.shields.io/badge/license-" +
+          encodeURIComponent(l.id).replace("-", "--") +
+          "-blue.svg)](" +
           l.url +
-          ')'
+          ")"
       );
-      newlines.push('This is distributed under the [' + l.name + '](' + l.url + ').');
+      newlines.push(
+        "This is distributed under the [" + l.name + "](" + l.url + ")."
+      );
       inserted = true;
     };
     readme.split(/\r?\n/).forEach(line => {
@@ -98,14 +101,14 @@ module.exports = class extends Generator {
       if (match) {
         remove = true;
         level = match[1];
-        next = new RegExp('^' + level + '(?!#)');
+        next = new RegExp("^" + level + "(?!#)");
         return;
       }
       newlines.push(line);
     });
     insert();
-    this.fs.write('README.md', newlines.join('\n'));
+    this.fs.write("README.md", newlines.join("\n"));
 
-    this.fs.write('LICENSE', this.props.license.licenseText);
+    this.fs.write("LICENSE", this.props.license.licenseText);
   }
 };
